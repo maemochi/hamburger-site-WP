8a1f96b52e203a79e80af65b7418371704179805
maemochi <maemoch15@gmail.com>
2022-04-14 06:55:25 +0900

    headerからnav部分を削除

--- header.php
+++ header.php
@@ -16,34 +16,3 @@
                 <?php get_search_form(); ?>
         </header>
 
-        <nav class="l-sidebar p-sidebar c-scrollbar--hidden">
-            <h2 class="p-sidebar__menu">Menu</h2>
-            <button class="p-sidebar__button">
-            <span class="c-line c-line--deg45 p-sidebar__close"></span>
-            <span class="c-line c-line--deg135 p-sidebar__close"></span>
-            </button>
-                    <ul><h3 class="p-sidebar__main-menu">バーガー</h3>
-                        <li class="p-sidebar__sub-menu"><a href="#">ハンバーガー</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">チーズバーガー</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">テリヤキバーガー</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">アボカドバーガー</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">フィッシュバーガー</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">ベーコンバーガー</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">チキンバーガー</a></li>
-                    </ul>                
-                    <ul><h3 class="p-sidebar__main-menu">サイド</h3>
-                        <li class="p-sidebar__sub-menu"><a href="#">ポテト</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">サラダ</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">ナゲット</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">コーン</a></li>
-                    </ul>
-                
-                    <ul><h3 class="p-sidebar__main-menu">ドリンク</h3>
-                        <li class="p-sidebar__sub-menu"><a href="#">コーラ</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">ファンタ</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">オレンジ</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">アップル</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">紅茶（Ice/Hot）</a></li>
-                        <li class="p-sidebar__sub-menu"><a href="#">コーヒー（Ice/Hot）</a></li>
-                    </ul>
-        </nav>
