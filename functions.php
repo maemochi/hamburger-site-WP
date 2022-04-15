45de6dcce9d35f99b70d68f34b00501c91b46183
maemochi <maemoch15@gmail.com>
2022-04-16 06:47:44 +0900

    jQuery読み込み

--- functions.php
+++ functions.php
@@ -6,7 +6,11 @@ function add_files() {
     wp_enqueue_style('Google Fonts', 'https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;500;700;800;900&family=Roboto:wght@400;500;700;900&display=swap');
     // メインのCSSファイル
     wp_enqueue_style('main-style', get_stylesheet_uri());
+
+	// WordPress提供のjquery.jsを読み込まない
     // JavaScriptファイル
+	wp_deregister_script('jquery');
+    wp_enqueue_script('jquery','//code.jquery.com/jquery-3.6.0.min.js','','',true );
     wp_enqueue_script('main-script', get_theme_file_uri().'/js/script.js', array(), '', true);
 }
 // add_filesを呼び出す
