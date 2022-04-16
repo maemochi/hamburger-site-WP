84b200ff3e3ca8526710c1c39eb880a29c58c163
maemochi <maemoch15@gmail.com>
2022-04-16 09:22:43 +0900

    scssコンパイル先の設定、style.cssをルートディレクトリに

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function add_files() {
     // Google Fonts
     wp_enqueue_style('Google Fonts', 'https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;500;700;800;900&family=Roboto:wght@400;500;700;900&display=swap');
     // メインのCSSファイル
-    wp_enqueue_style('main-style', get_stylesheet_uri());
+    wp_enqueue_style('main-style', get_stylesheet_uri('/css/style.css'));
 
 	// WordPress提供のjquery.jsを読み込まない
     // JavaScriptファイル
