faf99907104ddebda1c5e439302fa505ede4d9bf
maemochi <maemoch15@gmail.com>
2022-05-08 16:30:47 +0900

    エスケープ処理追加

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <!-- 検索フォームを表示するためのテンプレート -->
 
-<form class="p-search-form"  method="get" action="<?php echo home_url('/'); ?>">
+<form class="p-search-form"  method="get" action="<?php echo esc_url( home_url('/') ); ?>">
     <input type="search" name="s" id="s" value="" class="c-search-box p-search-form__search-box">
     <button class="c-button p-search-form__button" type="submit">検索</button>
 </form>
